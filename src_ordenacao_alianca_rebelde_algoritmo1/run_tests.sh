bbd80942bf453c9a371087695a27450fa6e5c0d8
Luisgsilva950 <luisgsilva950@gmail.com>
2020-10-19 14:44:04 -0300

    improve

--- src_ordenacao_alianca_rebelde_algoritmo1/run_tests.sh
+++ src_ordenacao_alianca_rebelde_algoritmo1/run_tests.sh
@@ -3,8 +3,8 @@ TMP_OUT=$2
 
 for i in {0..9..1}; do
   testname=$(printf "%d" "$i")
-  $EXEC < tests/$testname.in > $TMP_OUT
-  if ! diff -qwB tests/$testname.out $TMP_OUT &>/dev/null; then
+  $EXEC < ../tests/$testname.in > $TMP_OUT
+  if ! diff -qwB ../tests/$testname.out $TMP_OUT &>/dev/null; then
     echo "Test $testname failed"
   else
     echo "Test $testname passed"
