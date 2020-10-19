164eadcdfc2dddd76ec1f1024d169d59a4839088
Luisgsilva950 <luisgsilva950@gmail.com>
2020-10-19 14:18:39 -0300

    fix merge sort

--- src_ordenacao_alianca_rebelde_algoritmo2/main.cc
+++ src_ordenacao_alianca_rebelde_algoritmo2/main.cc
@@ -5,65 +5,54 @@
 
 using namespace std;
 
-void swap(vector<Civilization> &v, int x, int y) {
-    Civilization temp = v[x];
-    v[x] = v[y];
-    v[y] = temp;
-}
-
 bool mustSwap(const Civilization &c1, const Civilization &c2) {
-    if (c1.getDistance() > c2.getDistance()) return true;
-    else if (c1.getDistance() < c2.getDistance()) return false;
+    if (c1.getDistance() < c2.getDistance()) return true;
+    else if (c1.getDistance() > c2.getDistance()) return false;
     else {
         if (c1.getSize() > c2.getSize()) return true;
         else return false;
     }
 }
 
-void merge(vector<Civilization> &vec, int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    Civilization L[n1], R[n2];
 
-    for (int i = 0; i < n1; i++)
-        L[i] = vec[l + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = vec[m + 1 + j];
-
-    int i = 0;
-    int j = 0;
-    int k = l;
-
-    while (i < n1 && j < n2) {
-        if (mustSwap(L[i], R[j])) {
-            vec[k] = L[i];
+void merge(vector<Civilization> &vec, int low, int high, int mid) {
+    int i, j, k;
+    Civilization c[vec.size()];
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high) {
+        if (mustSwap(vec[i], vec[j])) {
+            c[k] = vec[i];
+            k++;
             i++;
         } else {
-            vec[k] = R[j];
+            c[k] = vec[j];
+            k++;
             j++;
         }
-        k++;
     }
-
-    while (i < n1) {
-        vec[k] = L[i];
+    while (i <= mid) {
+        c[k] = vec[i];
+        k++;
         i++;
-        k++;
     }
-
-    while (j < n2) {
-        vec[k] = R[j];
-        j++;
+    while (j <= high) {
+        c[k] = vec[j];
         k++;
+        j++;
+    }
+    for (i = low; i < k; i++) {
+        vec[i] = c[i];
     }
 }
 
 void mergeSort(vector<Civilization> &vec, int L, int R) {
     if (L < R) {
-        int m = L + (R - L) / 2;
+        int m = (L + R) / 2;
         mergeSort(vec, L, m);
         mergeSort(vec, m + 1, R);
-        merge(vec, L, m, R);
+        merge(vec, L, R, m);
     }
 }
 
@@ -77,7 +66,7 @@ int main() {
     while (scanf("%s %i %i", &input, &distance, &size) != EOF) {
         civilizations.push_back(new Civilization(input, distance, size));
     }
-    int MAX_LENGTH = civilizations.size();
+    int MAX_LENGTH = civilizations.size() - 1;
     mergeSort(civilizations, 0, MAX_LENGTH);
     for (auto &&civilization : civilizations) {
         cout << civilization.getName().c_str() << " " << civilization.getDistance() << " " << civilization.getSize()
