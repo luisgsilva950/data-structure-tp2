0f2207ca5ab0e7cb4670bb76f712290ea24522ca
Luisgsilva950 <luisgsilva950@gmail.com>
2020-10-19 20:15:11 -0300

    heap sort and add more tests

--- src_ordenacao_alianca_rebelde_algoritmo2/main.cc
+++ src_ordenacao_alianca_rebelde_algoritmo2/main.cc
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+
+void swap(vector<Civilization> &v, int x, int y) {
+    Civilization temp = v[x];
+    v[x] = v[y];
+    v[y] = temp;
+}
+
 bool mustSwap(const Civilization &c1, const Civilization &c2) {
     if (c1.getDistance() < c2.getDistance()) return true;
     else if (c1.getDistance() > c2.getDistance()) return false;
@@ -14,72 +21,31 @@ bool mustSwap(const Civilization &c1, const Civilization &c2) {
     }
 }
 
-void heapify(vector<Civilization> &vec, int n, int i) {
+void buildHeap(vector<Civilization> &vec, int n, int i) {
     int largest = i;
     int l = 2 * i + 1;
     int r = 2 * i + 2;
-    if (l < n && mustSwap(vec[l], vec[largest]))
+    if (l < n && !mustSwap(vec[l], vec[largest]))
         largest = l;
 
-    if (r < n && mustSwap(vec[r], vec[largest]))
+    if (r < n && !mustSwap(vec[r], vec[largest]))
         largest = r;
+
     if (largest != i) {
-        swap(vec[i], vec[largest]);
-        heapify(vec, n, largest);
+        swap(vec, i, largest);
+        buildHeap(vec, n, largest);
     }
 }
 
 
 void heapSort(vector<Civilization> &vec, int n) {
     for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(vec, n, i);
+        buildHeap(vec, n, i);
     for (int i = n - 1; i >= 0; i--) {
-        swap(vec[0], vec[i]);
-        heapify(vec, i, 0);
+        swap(vec, 0, i);
+        buildHeap(vec, i, 0);
     }
 }
-
-
-//void merge(vector<Civilization> &vec, int low, int high, int mid) {
-//    int i, j, k;
-//    Civilization c[vec.size()];
-//    i = low;
-//    k = low;
-//    j = mid + 1;
-//    while (i <= mid && j <= high) {
-//        if (mustSwap(vec[i], vec[j])) {
-//            c[k] = vec[i];
-//            k++;
-//            i++;
-//        } else {
-//            c[k] = vec[j];
-//            k++;
-//            j++;
-//        }
-//    }
-//    while (i <= mid) {
-//        c[k] = vec[i];
-//        k++;
-//        i++;
-//    }
-//    while (j <= high) {
-//        c[k] = vec[j];
-//        k++;
-//        j++;
-//    }
-//    for (i = low; i < k; i++) {
-//        vec[i] = c[i];
-//    }
-//}
-//
-//void mergeSort(vector<Civilization> &vec, int L, int R) {
-//    if (L < R) {
-//        int m = (L + R) / 2;
-//        mergeSort(vec, L, m);
-//        mergeSort(vec, m + 1, R);
-//        merge(vec, L, R, m);
-//    }
-//}
 
 int main() {
     int N = 0;
@@ -91,7 +57,7 @@ int main() {
     while (scanf("%s %i %i", &input, &distance, &size) != EOF) {
         civilizations.push_back(new Civilization(input, distance, size));
     }
-    int MAX_LENGTH = civilizations.size() - 1;
+    int MAX_LENGTH = civilizations.size();
     heapSort(civilizations, MAX_LENGTH);
     for (auto &&civilization : civilizations) {
         cout << civilization.getName().c_str() << " " << civilization.getDistance() << " " << civilization.getSize()
